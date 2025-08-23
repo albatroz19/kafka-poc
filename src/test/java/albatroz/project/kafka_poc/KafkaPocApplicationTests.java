2064f3abbe5a44ed1582413867ae5c7bc72bcf26
Mohit Bisht <mohit.bisht@tothenew.com>
2025-08-23 22:11:58 +0530

    Kafka POC

---
+++ src/test/java/albatroz/project/kafka_poc/KafkaPocApplicationTests.java
@@ -0,0 +1,13 @@
+package albatroz.project.kafka_poc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaPocApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
